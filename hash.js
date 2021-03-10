4f767cd9c79157675b99ee2d861c81a2fbebc0f0
Your Name <you@example.com>
2021-03-09 20:12:38 -0600

    :tada: Implementation to Bcrypt

---
+++ hash.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcrypt')
+
+let saltRounds = 10
+
+
+// YOU WORD
+// 
+// 
+let myString = 'mysecretword'
+
+
+// BRYPT BLOCK
+// 
+// 
+bcrypt.hash(myString, saltRounds, (err, hash) => {
+    if(!err) {
+        console.log(hash)
+    } else {
+        console.log('Error', err)
+    }
+})
+
+
+
+// to compare, comment out the BRYPT BLOCK 
+// insert your word and console password 
+// Run node hash.js
+// Result will be true or false
+// 
+// bcrypt.compare('mysecretword', '$2b$10$i3bzBN91cAWu7p3LaH1NxOPaBqIM8m3dxTfSKvUmAg7jkNflnHEXy',
+// (err, res) => {
+//     if(!err) {
+//         console.log('Password Correct:', res)
+//     } else {
+//         console.log('Error', err)
+//     }
+// })
+
+
+
+//node hash.js
